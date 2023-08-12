743ea97c6c5a1264ea55363336b5274be20105ef
Felipe Barbosa do Nascimento <dfelipebdn@gmail.com>
2023-08-12 09:21:04 -0300

    - This patch might be necessary to ensure that each "Org" record has a unique "whatsapp" value, preventing duplicates in the database.

---
+++ prisma/migrations/20230812121909_/migration.sql
@@ -0,0 +1,66 @@
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "collar" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "energy_level" INTEGER NOT NULL,
+    "size" TEXT NOT NULL,
+    "age" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "independence" TEXT NOT NULL,
+    "anvironment" TEXT NOT NULL,
+    "org_id" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "images" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "pet_id" TEXT NOT NULL,
+
+    CONSTRAINT "images_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orgs" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "organization" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "cep" INTEGER NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+
+    CONSTRAINT "orgs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "requirements" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "pet_id" TEXT NOT NULL,
+
+    CONSTRAINT "requirements_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "pets_collar_key" ON "pets"("collar");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orgs_email_key" ON "orgs"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orgs_whatsapp_key" ON "orgs"("whatsapp");
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_org_id_fkey" FOREIGN KEY ("org_id") REFERENCES "orgs"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "images" ADD CONSTRAINT "images_pet_id_fkey" FOREIGN KEY ("pet_id") REFERENCES "pets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "requirements" ADD CONSTRAINT "requirements_pet_id_fkey" FOREIGN KEY ("pet_id") REFERENCES "pets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
