497f55cef65dc258527840d6e87bbb9ba10f2084
Felipe Barbosa <dfelipebdn@gmail.com>
2023-09-19 17:13:35 -0300

    changed config vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  build: {
+    target: 'es2020',
+  },
   plugins: [tsconfigPaths()],
   test: {
     environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
