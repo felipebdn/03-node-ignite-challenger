c2996b636d1213193bbffd22d69a9d386712d69f
Felipe Nascimento <yami@Felipes-Mac-Pro.local>
2023-09-25 23:40:13 -0300

    feat: Add getInfoPetSchema for retrieving pet information

--- src/http/controllers/not-authenticated-routes/routes.ts
+++ src/http/controllers/not-authenticated-routes/routes.ts
@@ -6,11 +6,12 @@ import { GetInfoPet } from './get-info-pet'
 import { refreshToken } from './refresh-token'
 import { orgsRegisterSchema } from '@/swagger-schemas/orgs-register'
 import { orgAuthenticateSchema } from '@/swagger-schemas/org-authenticate'
+import { getInfoPetSchema } from '@/swagger-schemas/get-info-pet-schema'
 
 export async function notAuthRoutes(app: FastifyInstance) {
   app.post('/orgs', orgsRegisterSchema, orgRegister)
   app.post('/sessions', orgAuthenticateSchema, orgAuthenticate)
   app.get('/pets', FetchPets)
-  app.get('/pet/:id', GetInfoPet)
+  app.get('/pet/:id', getInfoPetSchema, GetInfoPet)
   app.patch('/token/refresh', refreshToken)
 }
---
+++ src/swagger-schemas/get-info-pet-schema.ts
@@ -0,0 +1,60 @@
+import { RouteShorthandOptions } from 'fastify'
+
+export const getInfoPetSchema: RouteShorthandOptions = {
+  schema: {
+    description: 'Essa rota é responsável mostrar as informações de um pet',
+    summary: 'Bunscar Pet',
+    tags: ['Pets'],
+    querystring: {
+      type: 'object',
+      required: ['id'],
+      properties: {
+        id: { type: 'string' },
+      },
+    },
+    response: {
+      201: {
+        type: 'object',
+        properties: {
+          pet: {
+            type: 'object',
+            properties: {
+              id: { type: 'string' },
+              name: { type: 'string' },
+              energy_level: { type: 'number' },
+              size: { type: 'string' },
+              age: { type: 'string' },
+              description: { type: 'string' },
+              requirements: { type: 'string' },
+              independence: { type: 'string' },
+              anvironment: { type: 'string' },
+              org_id: { type: 'string' },
+            },
+          },
+          images: {
+            type: 'array',
+            id: { type: 'string' },
+            url: { type: 'string' },
+            pet_id: { type: 'string' },
+          },
+
+          org: {
+            type: 'object',
+            properties: {
+              id: { type: 'string' },
+              name: { type: 'string' },
+              organization: { type: 'string' },
+              road: { type: 'string' },
+              number: { type: 'string' },
+              sector: { type: 'string' },
+              city: { type: 'string' },
+              state: { type: 'string' },
+              cep: { type: 'number' },
+              whatsapp: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+  },
+}
