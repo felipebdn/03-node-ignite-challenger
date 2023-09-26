e307b270a0bc1eeba0ea264401d17eca671f82fc
Felipe Barbosa <dfelipebdn@gmail.com>
2023-09-26 14:18:31 -0300

    criado teste unitario do caso de uso de update pet

---
+++ test/use-cases/update-pet.spec.ts
@@ -0,0 +1,52 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
+import { UpdatePetUseCase } from '@/use-cases/update-pet'
+
+let inMemoryPetsRepository: InMemoryPetsRepository
+let sut: UpdatePetUseCase
+
+describe('Update Pet Use Case', () => {
+  beforeEach(() => {
+    inMemoryPetsRepository = new InMemoryPetsRepository()
+    sut = new UpdatePetUseCase(inMemoryPetsRepository)
+  })
+  it('should be able to register pet', async () => {
+    const createPet = await inMemoryPetsRepository.create({
+      name: 'example',
+      energy_level: 5,
+      size: 'medium',
+      age: 'adolescent',
+      description: 'empty',
+      independence: 'medium',
+      environment: 'Lugares fechados',
+      org_id: 'org_id',
+      requirements: 'carinho',
+    })
+    const { pet } = await sut.execute({
+      data: {
+        name: 'example',
+        energy_level: 3,
+        size: 'big',
+        age: 'cub',
+        description: 'empty',
+        independence: 'low',
+        environment: 'Lugares fechados',
+        requirements: 'carinho',
+      },
+      petId: createPet.id,
+    })
+
+    expect(pet).toEqual(
+      expect.objectContaining({
+        name: 'example',
+        energy_level: 3,
+        size: 'big',
+        age: 'cub',
+        description: 'empty',
+        independence: 'low',
+        environment: 'Lugares fechados',
+        requirements: 'carinho',
+      }),
+    )
+  })
+})
